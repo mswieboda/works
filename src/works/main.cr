342659f1da07e0b700d0decf1b5c9d12c68e9274
Matt Swieboda <mswieboda@users.noreply.github.com>
2022-08-11 19:45:57 -0500

    copy main of cpp_game

--- src/works/main.cr
+++ src/works/main.cr
@@ -1,4 +1,16 @@
 module Works
+  module Allegro
+    # Allegro constants not in bindings
+    MagLinear = 0x0080
+    MinLinear = 0x0040
+    Suggest = 2
+  end
+
+  FPS = 60
+  Width = 1024
+  Height = 768
+  Name = "works"
+
   class Main
     def check_init(test, description)
       return if test
@@ -13,59 +25,58 @@ module Works
       filename = "assets/player.png"
 
       check_init(CrystalAllegro.init, "crystal allegro")
-
-      LibAllegro.set_new_display_adapter(ARGV[1].to_i) if ARGV.size > 1
-
-      check_init(LibAllegro.install_mouse, "mouse")
       check_init(LibAllegro.install_keyboard, "keyboard")
+      check_init(LibAllegro.install_mouse, "mouse")
+      check_init(LibAllegro.init_font_addon, "font addon")
+      # check_init(LibAllegro.init_ttf_addon, "ttf addon") # need to add to bindings
       check_init(LibAllegro.init_image_addon, "image addon")
+      check_init(LibAllegro.init_primitives_addon, "primitives addon")
 
-      display = LibAllegro.create_display(640, 480)
+      timer = LibAllegro.create_timer(1.0 / FPS)
+      check_init(timer, "timer")
 
+      queue = LibAllegro.create_event_queue
+      check_init(queue, "queue")
+
+      # display options for anti-aliasing
+      LibAllegro.set_new_display_option(LibAllegro::SampleBuffers, 1, Allegro::Suggest)
+      LibAllegro.set_new_display_option(LibAllegro::Samples, 8, Allegro::Suggest)
+      LibAllegro.set_new_bitmap_flags(Allegro::MinLinear | Allegro::MagLinear)
+
+      display = LibAllegro.create_display(Width, Height)
       check_init(display, "display")
 
-      LibAllegro.set_window_title(display, filename)
+      LibAllegro.set_window_title(display, Name)
 
-      t0 = LibAllegro.get_time
-      bitmap = LibAllegro.load_bitmap(filename)
-      t1 = LibAllegro.get_time
+      # set to fullscreen windowed
+      # al_set_display_flag(display, ALLEGRO_FULLSCREEN_WINDOW, true);
 
-      check_init(bitmap, "bitmap")
-
-      puts "Loading took #{t1 - t0} seconds"
-
-      timer = LibAllegro.create_timer(1.0 / 30)
-      queue = LibAllegro.create_event_queue
+      # al_hide_mouse_cursor(display);
 
       LibAllegro.register_event_source(queue, LibAllegro.get_keyboard_event_source)
+      LibAllegro.register_event_source(queue, LibAllegro.get_mouse_event_source)
       LibAllegro.register_event_source(queue, LibAllegro.get_display_event_source(display))
       LibAllegro.register_event_source(queue, LibAllegro.get_timer_event_source(timer))
-      LibAllegro.start_timer(timer) # Start the timer
 
-      # Primary 'game' loop.
+      redraw = false
       event = LibAllegro::Event.new
 
+      LibAllegro.start_timer(timer)
+
       loop do
-        LibAllegro.wait_for_event(queue, pointerof(event)) # Wait for and get an event.
+        LibAllegro.wait_for_event(queue, pointerof(event))
+
+        # sceneManager.update(event)
+
+        # if (sceneManager.isExit)
+        #   break
 
         case event.type
         when LibAllegro::EventDisplayClose
           break
-        when LibAllegro::EventKeyChar
-          # Use keyboard to zoom image in and out.
-          # 1: Reset zoom.
-          # +: Zoom in 10%
-          # -: Zoom out 10%
-          # f: Zoom to width of window
-          break if event.keyboard.keycode == LibAllegro::KeyEscape # Break the loop and quite on escape key.
-          case event.keyboard.unichar
-            when '1' then zoom = 1.0
-            when '+' then zoom *= 1.1
-            when '-' then zoom /= 1.1
-            when 'f' then zoom = LibAllegro.get_display_width(display).to_f / LibAllegro.get_bitmap_width(bitmap)
-          end
+        when LibAllegro::EventKeyDown
+          break if event.keyboard.keycode == LibAllegro::KeyEscape
         when LibAllegro::EventTimer
-          # Trigger a redraw on the timer event
           redraw = true
         end
 
@@ -73,20 +84,15 @@ module Works
         if redraw && LibAllegro.is_event_queue_empty(queue)
           redraw = false
 
-          # Clear so we don't get trippy artifacts left after zoom.
           LibAllegro.clear_to_color(LibAllegro.map_rgb_f(0, 0, 0))
 
-          if zoom == 1
-            LibAllegro.draw_bitmap(bitmap, 0, 0, 0)
-          else
-            LibAllegro.draw_scaled_rotated_bitmap(bitmap, 0, 0, 0, 0, zoom, zoom, 0, 0)
-          end
+          # sceneManager.draw()
 
           LibAllegro.flip_display
         end
       end
 
-      LibAllegro.destroy_bitmap(bitmap)
+      # sceneManager.destroy()
     end
   end
 end
